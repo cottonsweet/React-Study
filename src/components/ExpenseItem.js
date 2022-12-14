7e1d0bc71770721609d5c1dd9f661216d9ff71e4
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-14 23:31:16 +0900

    JSX에서 동적 데이터 출력

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 // 컴포넌트 작명 규칙 무조건 앞글자는 대문자로 작명한다.
 // 한 단어 안에 여러단어를 결합할 경우 그 다음 단어 또한 대문자로 즉 파스칼 케이스 문법처럼 해야함
 
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 11, 14);
+  const expenseTitle = "M1 맥북프로 16인치";
+  const expenseAmount = 1200;
+
   return (
-    <div>
-      <div>날짜</div>
-      <div>
-        <h2>제목</h2>
-        <div>금액</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount} 원</div>
       </div>
     </div>
   );
