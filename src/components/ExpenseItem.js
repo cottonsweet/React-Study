b486cf6df5b02e365e13bb1affa8f7b45dbc4d00
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-15 23:19:38 +0900

    컴포넌트 정리

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,16 +3,17 @@
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem({ date, title, amount }) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount} 원</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
