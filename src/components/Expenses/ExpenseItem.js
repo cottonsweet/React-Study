a4e3dbf3180a9812d6dffc9046aeec414ddab01c
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-16 01:07:57 +0900

    React State에 대해 살펴보기

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  // React는 State의 변경되는 값에 따라 컴포넌트가 렌더링 된다.
+  const clickHandler = () => {
+    setTitle("dd");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
