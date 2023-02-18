15284916cb4e833036bfc8a2d41d8cbc2e84300f
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-18 20:06:07 +0900

    명령문 반환 추가 및 코드 리펙토링

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">데이터가 없습니다.</h2>;
+  }
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        /**
+         *  key props을 추가하는 이유는 모든아이템을 식별할수 있으며
+         * 배열길이, 아이템이 위치해야할 곳까지 인식함 좀 더 효율적인 방법으로 업데이트를 할수가 있음
+         */
+        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
