20e286d05ae732e4169425b048aa81c42fdd7f4f
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-18 19:06:05 +0900

    데이터 추가

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -54,18 +54,19 @@ const ExpenseForm = (props) => {
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls"></div>
-      <div className="new-expense__control">
-        <label>Title</label>
-        <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
-      </div>
-      <div className="new-expense__control">
-        <label>Amount</label>
-        <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
-      </div>
-      <div className="new-expense__control">
-        <label>Date</label>
-        <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+        </div>
       </div>
       <div className="new-expense__actions">
         <button>Add Expense</button>
