b2e7344f06c0f562d23f44db8c5a002070cfcb92
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-18 20:49:33 +0900

    ExpenseForm 컴포넌트 버그수정

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -9,8 +9,9 @@ const ExpensesList = (props) => {
     <ul className="expenses-list">
       {props.items.map((expense) => (
         /**
-         *  key props을 추가하는 이유는 모든아이템을 식별할수 있으며
+         * key props을 추가하는 이유는 모든아이템을 식별할수 있으며
          * 배열길이, 아이템이 위치해야할 곳까지 인식함 좀 더 효율적인 방법으로 업데이트를 할수가 있음
+         * React가 목록 요소를 올바르게 식별 및 업데이트(필요시) 하는데 필요하기 때문이라고 보면 됨
          */
         <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
       ))}
