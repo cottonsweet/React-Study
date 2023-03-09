0c556c49a7fff8a68d2e81bb47c8228a4bcbd1d6
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-09 21:50:02 +0900

    사용자 정의 컨텍스트 제공자 구성요소 빌드 및 사용

--- 02-starting-project/src/components/Login/Login.js
+++ 02-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 /**
  * 리듀서 함수는 컴포넌트 함수 바깥에 생성이 되었는데, 이 이유는 리듀서 함수 내부에서는
@@ -74,6 +75,8 @@ const Login = (props) => {
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: "", isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: "", isValid: null });
 
+  const authCtx = useContext(AuthContext);
+
   useEffect(() => {
     console.log("EFFECT RUNNING");
 
@@ -117,7 +120,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
