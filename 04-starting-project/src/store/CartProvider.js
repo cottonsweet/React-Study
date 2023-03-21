bfa9570509eb9fb6030634f5c4e4e469953b689f
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-21 23:42:42 +0900

    장바구니 컨텍스트 추가

---
+++ 04-starting-project/src/store/CartProvider.js
@@ -0,0 +1,17 @@
+import CartContext from "./cart-context";
+
+// CartContext 데이터를 관리하고, 그 컨텍스트를 접근하려는 모든 컴포넌트에 제공함
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+};
+
+export default CartProvider;
