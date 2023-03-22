4151460f5676f8cedd43581b4730bd9dfa097c1e
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-22 21:44:32 +0900

    아이템을 이동식으로 만들기

--- 04-starting-project/src/store/CartProvider.js
+++ 04-starting-project/src/store/CartProvider.js
@@ -35,13 +35,30 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
+
   return defaultCartState;
 };
 
 // CartContext 데이터를 관리하고, 그 컨텍스트를 접근하려는 모든 컴포넌트에 제공함
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
