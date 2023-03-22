3ade1f9da2209d47a2c671ed7bf64ca5313bd954
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-22 22:05:14 +0900

    useEffect 훅 사용

--- 04-starting-project/src/components/Layout/HeaderCartButton.js
+++ 04-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
   // 리듀스는 내장 메소드이며 데이터 배열을 값 하나로 변환해주는 메소드다
-  const numberOfCartItems = cartCtx.items.reduce((carNumber, item) => {
+  const numberOfCartItems = items.reduce((carNumber, item) => {
     return carNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
