d6db68b235d6472825f196a243ffd0acf2eedc50
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-23 23:10:01 +0900

    자식 컴포넌트 재평가 자세히 살펴보기

--- 05-starting-setup/src/App.js
+++ 05-starting-setup/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
 
 function App() {
@@ -9,13 +10,13 @@ function App() {
   console.log("APP RUNNING");
 
   const toggleParagraphHandler = () => {
-    setShowParagraph((prev) => !prev);
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
