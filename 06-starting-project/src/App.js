860737b2bede08723fe1a57da2f2b71137d1d395
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-28 23:19:14 +0900

    로딩 및 데이터 state 처리하기

--- 06-starting-project/src/App.js
+++ 06-starting-project/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const res = await fetch("https://swapi.dev/api/films");
     const data = await res.json();
     const transformedMovies = data.results.map((movieData) => {
@@ -18,6 +20,7 @@ const App = () => {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +29,9 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no moives.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
