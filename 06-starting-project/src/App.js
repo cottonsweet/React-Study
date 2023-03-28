12a9da2ed128ef9211a558c0f7325ab6f440188f
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-28 23:11:56 +0900

    async/await 문법사용

--- 06-starting-project/src/App.js
+++ 06-starting-project/src/App.js
@@ -6,21 +6,19 @@ import "./App.css";
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
-  };
+  async function fetchMoviesHandler() {
+    const res = await fetch("https://swapi.dev/api/films");
+    const data = await res.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
 
   return (
     <React.Fragment>
